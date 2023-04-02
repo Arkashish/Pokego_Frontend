b1fe165520008671d619941b886f0e5511887f2e
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-04-02 18:26:36 +0530

    Added all codes for frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>PokeBattle</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ pages/battle.js
@@ -0,0 +1,264 @@
+import React, { useState, useEffect } from "react";
+import soketIOClient from "socket.io-client";
+import randomString from "randomstring";
+
+const socket = soketIOClient("http://localhost:8000", { autoConnect: false });
+
+export default function Battle({ pokemon, battlleId }) {
+	const [Battle, setBattle] = useState({
+		_id: null,
+		playerOne: {
+			pokemon: {},
+		},
+		playerTwo: {
+			pokemon: {},
+		},
+	});
+	useEffect(() => {
+		socket.connect();
+		socket.on("connect",async () => {
+			socket.emit("join", battlleId ? battlleId : randomString.generate());
+		})
+		// socket.emit("join", battlleId ? battlleId : randomString.generate());
+
+		socket.on("refresh",(Battle) => {
+			setBattle(Battle);
+		})
+	}, []);
+	return (
+		<div className="bg-blue-100 h-screen">
+			<h1 className="text-5xl font-bold text-center p-16">pokeBattle!</h1>
+			<h2 className="text-center">BattleId: {battlleId}</h2>
+			<div className="container mx-auto text-center">
+				<div className="flex space-x-24">
+					<div className="w-1/2">
+						<h1>player 1</h1>
+						<div className="flex flex-wrap">
+							{pokemon &&
+								pokemon.map((mon) => (
+									<div
+										key={mon._id}
+										className={`w-1/4  ${
+											Battle.playerOne.pokemon._id === mon._id
+												? "bg-green-200"
+												: ""
+										}`}
+										onClick={() => socket.emit("select", 1, mon)}
+									>
+										<img src={mon.Image} alt="pokemon" />
+										<h2 className="font-bold text-lg">{mon.name}</h2>
+									</div>
+								))}
+						</div>
+					</div>
+
+					<div className="w-1/2">
+						<h1>player 2</h1>
+						<div className="flex flex-wrap">
+							{pokemon &&
+								pokemon.map((mon) => (
+									<div
+										key={mon._id}
+										className={`w-1/4 ${
+											Battle.playerTwo.pokemon._id === mon._id
+												? "bg-green-200"
+												: ""
+										}`}
+										onClick={() => socket.emit("select", 2, mon)}
+									>
+										<img src={mon.Image} alt="pokemon" />
+										<h2 className="font-bold text-lg">{mon.name}</h2>
+									</div>
+								))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export async function getServerSideProps({ query }) {
+	const response = await fetch("http://localhost:8000/pokemon");
+	const data = await response.json();
+
+	const pokemon = JSON.parse(JSON.stringify(data));
+
+	return {
+		props: {
+			pokemon,
+			battlleId: query.battlleId || null,
+		},
+	};
+}
+// import React, { useState, useEffect } from "react";
+// import soketIoClient from "socket.io-client";
+// import randomString from "randomstring";
+
+// const socket = soketIoClient("http://localhost:8000", { autoConnect: false });
+
+// export default function battle({ pokemon, battleId }) {
+//     const [Battle, setBattle] = useState({
+//         _id: null,
+//         playerOne: {
+//             pokemon: {},
+//         },
+//         playerTwo: {
+//             pokemon: {},
+//         },
+//     });
+//     useEffect(() => {
+//         socket.connect();
+//         socket.on("connect",async () => {
+//             socket.emit("join", battleId ? battleId : randomString.generate());
+//         })
+//         socket.on("refresh",(battle)=>{
+//             setBattle(battle);
+//         })
+//     }, []);
+//     return (
+//         <div className="bg-blue-100 h-screen">
+//             <h1 className="text-5xl font-bold text-center p-16">PokeBattle!</h1>
+//             <h2 className="text-center">BattleId: {battleId}</h2>
+
+//             <div className="container mx-auto text-center">
+//                 <div className="flex space-x-24">
+//                     <div className="w-1/2">
+//                         <h1>Player 1</h1>
+//                         <div className="flex flex-wrap">
+//                             {pokemon &&
+//                                 pokemon.map( mon => (
+//                                     <div key={mon._id}
+//                                         className={"w-1/4" + (Battle.playerOne.pokemon._id === mon._id? 
+//                                         "bg-green-200":"")}
+//                                         onClick={() => socket.emit("select", 1, mon)}>
+//                                         <img src={mon.Image} alt="pokemon" />
+//                                         <h2 className="font-bold text-lg">{mon.name}</h2>
+//                                     </div>
+//                                 ))}
+//                         </div>
+//                     </div>
+//                     <div className="w-1/2">
+//                         <h1>Player 2</h1>
+//                         <div className="flex flex-wrap">
+//                             {pokemon &&
+//                                 pokemon.map((mon) => (
+//                                     <div
+//                                         key={mon._id}
+//                                         className={"w-1/4" + (Battle.playerTwo.pokemon._id === mon._id
+//                                             ? "bg-green-200"
+//                                             : ""
+//                                             )}
+//                                         onClick={() => socket.emit("select", 2, mon)}
+//                                     >
+//                                         <img src={mon.Image} alt="pokemon" />
+//                                         <h2 className="font-bold text-lg">{mon.name}</h2>
+//                                     </div>
+//                                 ))}
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export async function getServerSideProps({ query }) {
+//     const response = await fetch("http://localhost:8000/pokemon");
+//     const data = await response.json();
+
+//     const pokemon = JSON.parse(JSON.stringify(data));
+
+//     return {
+//         props: {
+//             pokemon,
+//             battleId: query.battleId || null,
+//         },
+//     };
+// }
+// import React, { useState, useEffect } from "react";
+// import soketIoClient from "socket.io-client";
+// import randomString from "randomstring";
+
+// const socket = soketIoClient("http://localhost:8000", { autoConnect: false });
+
+// export default function Battle({ pokemon, battlleId }) {
+// 	const [Battle, setBattle] = useState({
+// 		_id: null,
+// 		playerOne: {
+// 			pokemon: {},
+// 		},
+// 		playerTwo: {
+// 			pokemon: {},
+// 		},
+// 	});
+// 	useEffect(() => {
+// 		socket.connect();
+// 		socket.emit("join", battlleId ? battlleId : randomString.generate());
+// 	}, []);
+// 	return (
+// 		<div className="bg-blue-100 h-screen">
+// 			<h1 className="text-5xl font-bold text-center p-16">pokeBattle!</h1>
+// 			<h2 className="text-center">BattleId: {battlleId}</h2>
+// 			<div className="container mx-auto text-center">
+// 				<div className="flex space-x-24">
+// 					<div className="w-1/2">
+// 						<h1>player 1</h1>
+// 						<div className="flex flex-wrap">
+// 							{pokemon &&
+// 								pokemon.map((mon) => (
+// 									<div
+// 										key={mon._id}
+// 										className={`w-1/4  ${
+// 											(Battle.playerOne.pokemon._id === mon._id)
+// 												? "bg-green-200"
+// 												: ""
+// 										}`}
+// 										onClick={() => socket.emit("select", 1, mon)}
+// 									>
+// 										<img src={mon.Image} alt="pokemon" />
+// 										<h2 className="font-bold text-lg">{mon.name}</h2>
+// 									</div>
+// 								))}
+// 						</div>
+// 					</div>
+
+// 					<div className="w-1/2">
+// 						<h1>player 2</h1>
+// 						<div className="flex flex-wrap">
+// 							{pokemon &&
+// 								pokemon.map((mon) => (
+// 									<div
+// 										key={mon._id}
+// 										className={`w-1/4 ${
+// 											(Battle.playerTwo.pokemon._id === mon._id)
+// 												? "bg-green-200"
+// 												: ""
+// 										}`}
+// 										onClick={() => socket.emit("select", 2, mon)}
+// 									>
+// 										<img src={mon.Image} alt="pokemon" />
+// 										<h2 className="font-bold text-lg">{mon.name}</h2>
+// 									</div>
+// 								))}
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// export async function getServerSideProps({ query }) {
+// 	const response = await fetch("http://localhost:8000/pokemon");
+// 	const data = await response.json();
+
+// 	const pokemon = JSON.parse(JSON.stringify(data));
+
+// 	return {
+// 		props: {
+// 			pokemon,
+// 			battlleId: query.battlleId || null,
+// 		},
+// 	};
+// }
